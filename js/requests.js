65d8372df6bd96ed5db999114eefc0d08f48933c
brandonmrgich <58794109+brandonmrgich@users.noreply.github.com>
2022-01-24 13:28:51 -0500

    Cleaned requests.js

--- js/requests.js
+++ js/requests.js
@@ -1,21 +1,6 @@
 const urlBase = "https://contacts.ninja/LAMPAPI/contact";
 const extension = ".php";
 
-function callAPI() {
-    const a = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            let response = create();
-        }, 5000);
-    })
-        .then()
-        .then()
-        .catch();
-    // (resolve: (value: any) => void, reject: (reason?: any) => void): void
-
-    return a;
-}
-
-// Contacts
 async function makeRequest(endpoint, data = {}) {
     const res = await fetch(urlBase + "/" + endpoint + extension, {
         method: "POST",
