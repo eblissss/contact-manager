c69e81acae343e6c335ac33603a01a2f2380b251
eblissss <ebliss@knights.ucf.edu>
2022-01-26 18:29:22 -0500

    begin switch to new layout

--- js/contacts.js
+++ js/contacts.js
@@ -1,51 +1,6 @@
 let editing = false;
-let sav;
+let sav, msnry;
 const largeNum = 1000000000;
-
-class Contact extends HTMLElement {
-    constructor() {
-        // call super first ALWAYS
-        super();
-
-        // Make template (trying to get template from HTML is too quick for browser)
-        const template = document.createElement("template");
-        template.innerHTML = `<link href="css/main.css" rel="stylesheet" />
-        <div class="contactDiv">
-            <button class="btn btn-secondary mb-1" style="width: 100px">
-                Edit
-            </button>
-            <button class="btn btn-secondary mb-1" style="width: 100px">
-                Save
-            </button>
-            <button class="btn btn-secondary mb-1" style="width: 100px">
-                Delete
-            </button>
-            <br />
-            <slot name="c-fname" id="mycontact">NAME MISSING</slot>
-            <slot name="c-lname" id="mycontact">NAME MISSING</slot>
-            <ul>
-                <li><slot name="c-phone">PHONE MISSING</slot></li>
-                <li><slot name="c-email"></slot>EMAIL MISSING</li>
-            </ul>
-            <button class="btn btn-secondary mb-1" style="width: 100px">
-                Favorite
-            </button>
-        </div>`;
-
-        // Get template
-        //console.log(document.getElementById("contact-template"));
-        //const template = document.getElementById("contact-template");
-        const templateContent = template.content;
-
-        // Get shadow root
-        const shadowRoot = this.attachShadow({ mode: "open" });
-
-        // Append to shadow
-        shadowRoot.appendChild(templateContent.cloneNode(true));
-    }
-}
-// Define element so we can summon it
-customElements.define("contact-div", Contact);
 
 // Spawn a new contact on the div
 function spawnContact(
@@ -55,32 +10,40 @@ function spawnContact(
     phone,
     email,
     first = false,
-    isFavorite = 0
+    isFavorite = 0,
+    notes = "some notes about ...",
+    address = "1000 Ionic Drive"
 ) {
     // Create new contact
-    const contac = new Contact();
+    const contac = template.content.cloneNode(true).children[0];
     contac.id = "contact-" + id;
     contac.isFavorite = isFavorite;
     console.log(contac.id);
-    const content = contac.shadowRoot.children[1];
 
     // Add info
-    content.children[4].innerHTML = firstname;
-    content.children[5].innerHTML = lastname;
-    content.children[6].children[0].innerHTML = "Phone: " + phone;
-    content.children[6].children[1].innerHTML = "Email: " + email;
+    contac.children[1].innerHTML = firstname;
+    contac.children[2].innerHTML = lastname;
+    contac.children[3].children[0] = notes;
+    const infoSection = contac.children[3].children[1];
+    infoSection.children[0].innerHTML = `📞: <h4 class="phoneData">${phone}</h4>`;
+    infoSection.children[1].innerHTML = `📧: <h4 class="emailData">${email}</h4>`;
+    infoSection.children[2].innerHTML = `📍: <h4 class="addrData">${address}</h4>`;
 
+    const dotMenu = contac.children[4].children[1];
     // Add listeners
-    content.children[0].addEventListener("click", () => {
+    dotMenu.children[0].addEventListener("click", () => {
         if (!editing) {
             edit(contac);
         }
     });
-    content.children[2].addEventListener("click", () => {
+    dotMenu.children[1].addEventListener("click", () => {
         deleteContact(contac);
     });
-    content.children[7].addEventListener("click", () => {
-        setFavorite(contac);
+    // content.children[7].addEventListener("click", () => {
+    //     setFavorite(contac);
+    // });
+    contac.children[6].addEventListener("click", () => {
+        extend(contac);
     });
 
     // Insert into div
@@ -92,28 +55,36 @@ function spawnContact(
 
 // Update (or add) the contact
 function save(contac) {
-    content = contac.shadowRoot.children[1];
-
     // Grab slots
-    const fnameSlot = content.children[4];
-    const lnameSlot = content.children[5];
-    const phoneSlot = content.children[6].children[0];
-    const emailSlot = content.children[6].children[1];
+    const fnameSlot = contac.children[1];
+    const lnameSlot = contac.children[2];
+    const notesSlot = contac.children[3].children[0];
+
+    const infoSection = contac.children[3].children[1];
+
+    const phoneSlot = infoSection.children[0];
+    const emailSlot = infoSection.children[1];
+    const addrSlot = infoSection.children[2];
 
     // Grab info
     const firstname = fnameSlot.children[0].value;
     const lastname = lnameSlot.children[0].value;
+    const notes = notesSlot.children[0].value;
     const phoneNum = phoneSlot.children[0].value;
     const emailAddr = emailSlot.children[0].value;
+    const address = addrSlot.children[0].value;
+
     const id = contac.id.substring(8);
 
-    console.log(id, firstname, lastname, phoneNum, emailAddr);
+    console.log(id, firstname, lastname, notes, phoneNum, emailAddr, address);
 
     // Insert info back in
-    fnameSlot.innerHTML = firstname;
-    lnameSlot.innerHTML = lastname;
-    phoneSlot.innerHTML = "Phone: " + phoneNum;
-    emailSlot.innerHTML = "Email: " + emailAddr;
+    contac.children[1].innerHTML = firstname;
+    contac.children[2].innerHTML = lastname;
+    contac.children[3].children[0].innerHTML = notes;
+    contac.children[3].children[1].children[0].innerHTML = `📞: <h4 class="phoneData">${phoneNum}</h4>`;
+    contac.children[3].children[1].children[1].innerHTML = `📧: <h4 class="emailData">${emailAddr}</h4>`;
+    contac.children[3].children[1].children[2].innerHTML = `📍: <h4 class="addrData">${address}</h4>`;
 
     // Save data
     data = {
@@ -123,6 +94,7 @@ function save(contac) {
         email: emailAddr,
         phone: phoneNum,
         isFavorite: 0,
+        // ADD THE OTHER INFO
     };
 
     // Create or Update
@@ -146,28 +118,35 @@ function save(contac) {
         });
     }
 
-    content.children[1].removeEventListener("click", sav);
+    infoSection.children[3].remove();
     editing = false;
 }
 
 // Put contact in edit mode
 function edit(contac) {
+    extend(contac, (stay = true));
     // HIDE EDITING OPTION
 
     editing = true;
-    content = contac.shadowRoot.children[1];
 
     // Get slots
-    const fnameSlot = content.children[4];
-    const lnameSlot = content.children[5];
-    const phoneSlot = content.children[6].children[0];
-    const emailSlot = content.children[6].children[1];
+    const fnameSlot = contac.children[1];
+    const lnameSlot = contac.children[2];
+    const notesSlot = contac.children[3].children[0];
+
+    const infoSection = contac.children[3].children[1];
+
+    const phoneSlot = infoSection.children[0];
+    const emailSlot = infoSection.children[1];
+    const addrSlot = infoSection.children[2];
 
     // Get slot info
     const firstname = fnameSlot.innerText;
     const lastname = lnameSlot.innerText;
-    const phoneNum = phoneSlot.innerText.substring(7);
-    const emailAddr = emailSlot.innerText.substring(7);
+    const notes = notesSlot.innerText;
+    const phoneNum = phoneSlot.innerText.substring(3);
+    const emailAddr = emailSlot.innerText.substring(3);
+    const address = addrSlot.innerText.substring(3);
 
     // Replace text with inputs
     fnameSlot.innerHTML = `<input type="text" />`;
@@ -175,19 +154,30 @@ function edit(contac) {
     lnameSlot.innerHTML = `<input type="text" />`;
     lnameSlot.children[0].value = lastname;
 
+    notesSlot.innerHTML = `Notes: <input type="text" />`;
+    notesSlot.children[0].value = notes;
+
     phoneSlot.innerHTML = `Phone: <input type="text" />`;
     phoneSlot.children[0].value = phoneNum;
 
     emailSlot.innerHTML = `Email: <input type="text" />`;
     emailSlot.children[0].value = emailAddr;
 
-    //console.log(content);
-    content.children[1].addEventListener(
+    addrSlot.innerHTML = `Address: <input type="text" />`;
+    addrSlot.children[0].value = address;
+
+    // Add save button (should probably just show/hide instead of creating each time)
+    const sabeButton = document.createElement("button");
+    sabeButton.classList.add("btn");
+    sabeButton.style.backgroundColor = "white";
+    sabeButton.innerHTML = "SAVE";
+    sabeButton.addEventListener(
         "click",
         (sav = () => {
             save(contac);
         })
     );
+    infoSection.appendChild(sabeButton);
 }
 
 // Delete contact
@@ -233,6 +223,24 @@ function setFavorite(contac) {
     });
 }
 
+function extend(contac, stay = false) {
+    if (contac.classList.contains("extended") && !stay) {
+        console.log("shortening");
+        contac.style.height = "200px";
+        contac.children[3].style.height = "100px";
+        contac.children[3].children[1].style.display = "none";
+        contac.classList.remove("extended");
+    } else {
+        contac.style.height = "300px";
+        contac.children[3].style.height = "200px";
+        contac.children[3].children[1].style.display = "flex";
+        if (!contac.classList.contains("extended")) {
+            contac.classList.add("extended");
+        }
+    }
+    msnry.layout();
+}
+
 window.onload = function () {
     makeRequest("getFavorites", { userId: userId }).then((res) => {
         //console.log(res);
@@ -255,4 +263,108 @@ window.onload = function () {
             console.log(res.error);
         }
     });
+
+    // for (let i = 0; i < 5; i++) {
+    //     spawnContact(
+    //         1000 + i,
+    //         "Benedict",
+    //         "Cucumberpatch",
+    //         "not much bruv",
+    //         "808080808" + i,
+    //         "jojo@gmail.com",
+    //         "1000 Ionic Drive"
+    //     );
+    // }
+
+    const grid = document.getElementById("contactPane");
+    msnry = new Masonry(grid, {
+        itemSelector: ".grid-item",
+        columnWidth: 400,
+        gutter: 10,
+    });
+    console.log(msnry.getItemElements());
 };
+
+const template = document.createElement("template");
+template.innerHTML = `
+    <div class="contactDiv grid-item">
+    <img
+        class="contact-image"
+        src="https://www.publicdomainpictures.net/pictures/120000/nahled/abstract-composition-14287573024pP.jpg"
+        alt=""
+    />
+    <h3 class="fname">NAME MISSING</h3>
+    <h3 class="lname">NAME MISSING</h3>
+    <div class="contactLowerBackground">
+        <h4 class="notes">Information Notes Information Notes</h4>
+        <div class="drop-info">
+            <h4 class="phone">Phone: (808)-808-8888</h4>
+            <h4 class="email">Email: joe@gmail.com</h4>
+            <h4 class="address">Address: sheeesh</h4>
+        </div>
+    </div>
+
+    <!-- Default dropright button -->
+    <div class="dropdown dropmenu dropend">
+        <button
+            class="btn btn-secondary"
+            type="button"
+            id="dropdownMenu2"
+            data-bs-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+        >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-three-dots-vertical triple-dots"
+                viewBox="0 0 16 16"
+            >
+                <path
+                    d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"
+                />
+            </svg>
+        </button>
+        <div
+            class="dropdown-menu dropdown-menu-dark"
+            aria-labelledby="dropdownMenu2"
+        >
+            <button class="dropdown-item" type="button">Edit</button>
+            <button class="dropdown-item" type="button">Delete</button>
+        </div>
+    </div>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        class="bi bi-bookmark-star fav"
+        viewBox="0 0 16 16"
+    >
+        <path
+            d="M7.84 4.1a.178.178 0 0 1 .32 0l.634 1.285a.178.178 0 0 0 .134.098l1.42.206c.145.021.204.2.098.303L9.42 6.993a.178.178 0 0 0-.051.158l.242 1.414a.178.178 0 0 1-.258.187l-1.27-.668a.178.178 0 0 0-.165 0l-1.27.668a.178.178 0 0 1-.257-.187l.242-1.414a.178.178 0 0 0-.05-.158l-1.03-1.001a.178.178 0 0 1 .098-.303l1.42-.206a.178.178 0 0 0 .134-.098L7.84 4.1z"
+        />
+        <path
+            d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"
+        />
+    </svg>
+    <!-- Show info button -->
+    <button class="dropButton" type="button">
+        <svg
+            color="white"
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-arrows-expand"
+            viewBox="0 0 16 16"
+        >
+            <path
+                fill-rule="evenodd"
+                d="M1 8a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 8zM7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708l2-2zM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10z"
+            />
+        </svg>
+    </button>
+    </div>`;
