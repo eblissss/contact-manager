18aa4fa94b3c98328ebd9f9a55854b2ddef586d3
eblissss <ebliss@knights.ucf.edu>
2022-01-31 13:44:15 -0500

    allow searching full name

--- LAMPAPI/contact/search.php
+++ LAMPAPI/contact/search.php
@@ -17,11 +17,22 @@
 	} 
 	else
 	{
+		// Create Search
+		$search = trim($inData["search"]);
+		$split = explode(' ', $search, 2);
+
+		$searchA = "%" . $search . "%";
+		$searchB = "%" . $split[0] . "%";
+		$searchC = "%" . $split[1] . "%";
+
 		// Create SQL statement to search contacts
-		$sqlsearch = "select * from Contacts where (FirstName like ? or LastName like ?) and UserID=?";
+		$sqlsearch = "select * from Contacts where 
+						(FirstName like ? or FirstName like ? or 
+					 	LastName like ? or LastName like ?) 
+						and UserID=?";
 		$stmt = $conn->prepare($sqlsearch);
-		$name = "%" . $inData["search"] . "%";
-		$stmt->bind_param("ssi", $name, $name, $inData["userId"]);
+		
+		$stmt->bind_param("ssssi", $searchA, $searchB, $searchA, $searchC, $inData["userId"]);
 		$stmt->execute();
 		
 		$result = $stmt->get_result();
